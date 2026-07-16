17ccb9b98e74258b5dcae8fee4c942c2a5951428
Nishad S L <sl.nishad@gmail.com>
2026-07-16 10:50:35 +0530

    16072610

--- works/chat/dist/sw.js
+++ works/chat/dist/sw.js
@@ -237,7 +237,7 @@ const STATIC_ASSETS = [
   'app-icon-192.png',
   'app-icon-512.png',
   'pwa-install.js',
-  'app-bundle.521b0b15.js',
+  'app-bundle.149b1e18.js',
   'inline-sw-register.js',
   'inline-broadcast-channel.js',
   'inline-version.js',
